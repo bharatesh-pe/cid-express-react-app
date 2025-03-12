a23acfc7e0ed0dc25b8a80ab44d7391ce00b6b36
gayathridevirs <rsgayathridevi376@gmail.com>
2025-03-12 19:25:22 +0530

    role module

--- backend/routes/roleRoutes.js
+++ backend/routes/roleRoutes.js
@@ -4,6 +4,7 @@ const roleController = require('../controllers/roleController');
 
 const { validate_token } = require("../helper/validations")
 router.post('/create_role', [validate_token], roleController.create_role);
+router.post('/get_all_roles', [validate_token], roleController.get_all_roles);
 router.post('/update_role', [validate_token], roleController.update_role);
 router.post('/delete_role', [validate_token], roleController.delete_role);
 
