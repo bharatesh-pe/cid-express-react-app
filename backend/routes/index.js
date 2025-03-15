d691c63d160926d911ec0efc602662e210fca4d7
Adarsh S <adarsh.suryavanshi@patterneffectslabs.com>
2025-03-15 10:24:27 +0530

    approvalLogsRoutes updated

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,10 +5,11 @@ const userRoutes = require('./userRoutes');
 const roleRoutes = require('./roleRoutes');
 const repositoryRoutes = require('./repositoryRoutes');
 const templateRoutes = require('./templateRoutes');
+const approvalLogsRoutes = require('./approvalLogsRoutes');
 router.use('/auth', authRoutes);
 router.use('/user', userRoutes);
 router.use('/role', roleRoutes);
 router.use('/repository', repositoryRoutes);
 router.use('/templates', templateRoutes);
-
+router.use("/ui_case",approvalLogsRoutes)
 module.exports = router;
