607ccf10f197142439d740c38f58524c58453572
UV <uvkrishna05@gmail.com>
2025-03-23 19:50:00 +0530

    get the pt_case,ui_case,ui_to_pt if the sys_status is ui_case

--- backend/controllers/templateDataController.js
+++ backend/controllers/templateDataController.js
@@ -2493,8 +2493,13 @@ exports.paginateTemplateDataForOtherThanMaster = async (req, res) => {
         const validSortBy = fields[sort_by] ? sort_by : 'id';
 
         if (sys_status !== null && sys_status !== undefined) {
-            whereClause['sys_status'] = sys_status;
+            if (sys_status === "ui_case") {
+                whereClause['sys_status'] = { [Op.in]: ['ui_case', 'pt_case', 'ui_to_pt'] };
+            } else {
+                whereClause['sys_status'] = sys_status;
+            }
         }
+
 
         const result = await DynamicTable.findAndCountAll({
             where: whereClause,
