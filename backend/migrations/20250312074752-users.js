18bd3d0708041608dbdf8b2bd1860710b8205706
UV <uvkrishna05@gmail.com>
2025-03-27 23:34:57 +0530

    user table mobile number column and superwiser designation column

--- backend/migrations/20250312074752-users.js
+++ backend/migrations/20250312074752-users.js
@@ -33,7 +33,7 @@ module.exports = {
           type: Sequelize.STRING,
           allowNull: true,
         },
-        cug_no: {
+        mobile: {
           type: Sequelize.STRING,
           allowNull: true,
         },
