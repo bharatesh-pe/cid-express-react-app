02396f8366419efee1f48b3802605e221fcfcbd9
Mukesh <mugesh415@gmail.com>
2025-04-08 14:05:58 +0530

    alert ux and navbar zindex issue

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,18 +1,38 @@
-import React from "react";
-import { AppBar, Toolbar, InputBase, IconButton, Box, TextField, InputAdornment } from "@mui/material";
+import React, { useState } from "react";
+import { AppBar, Toolbar, InputBase, IconButton, Box, TextField, InputAdornment, Badge, Button } from "@mui/material";
 import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import SearchIcon from "@mui/icons-material/Search";
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
 
 export default function Navbar() {
+
+    const [loading, setLoading] = useState(false);
+
+    const [notificationCount, setNotificationCount] = useState(2);
+
+    const [showAlertPage, setShowAlertPage] = useState(false);
+
+    const showAlertUX = ()=> {
+        setShowAlertPage(true);
+    }
+
+    const closeAlertPage = ()=> {
+        setShowAlertPage(false);
+    }
+
     return (
+        <>
         <AppBar
             position="sticky"
             sx={{
                 background: "#fff",
                 boxShadow: "none",
                 borderBottom: "1px solid #D0D5DD",
+                zIndex: 98,
             }}
+            inert={loading ? true : false}
         >
             <Toolbar
                 sx={{ justifyContent: "space-between", padding: '3px' }}
@@ -67,8 +87,10 @@ export default function Navbar() {
 
                 {/* Right: Notification */}
                 <Box>
-                    <IconButton size="small" aria-label="show notifications" color="inherit">
-                        <NotificationsNoneIcon sx={{color: '#212121', height: '25px', width: '25px'}} />
+                    <IconButton size="small" aria-label="show notifications" color="inherit" sx={{position: 'relative'}} onClick={showAlertUX}>
+                        <Badge badgeContent={notificationCount === 0 ? null : notificationCount} className="notificationBadge" overlap="circular">
+                            <NotificationsNoneIcon sx={{color: '#212121', height: '25px', width: '25px'}} />
+                        </Badge>
                     </IconButton>
                     <IconButton size="small" aria-label="show help" color="inherit">
                         <HelpOutlineIcon sx={{color: '#212121', height: '25px', width: '25px'}} />
@@ -77,5 +99,82 @@ export default function Navbar() {
 
             </Toolbar>
         </AppBar>
+
+        { showAlertPage &&
+            <>
+                <Box sx={{ position: 'fixed', top: '0', left: '0', right: '0', bottom: '0', background: 'rgba(0, 0, 0, 0.5)', zIndex: '99', }} />
+                <Slide direction="left" in={showAlertPage} mountOnEnter unmountOnExit>
+                    <Box
+                        inert={loading ? true : false} 
+                        sx={{
+                            position: 'fixed',
+                            top: 0,
+                            right: 0,
+                            height: '100%',
+                            width: 'calc(100% - 920px)',
+                            background: '#F5F5F5',
+                            zIndex: 99,
+                            overflow: 'hidden',
+                        }}
+                    >
+                        <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center' ,height: '55.5px', padding: '3px 16px', borderBottom: '1px solid #EEEEEE', }}>
+
+                            <p style={{fontSize: '15px', color: '#000000', fontWeight: '500', fontFamily: 'Inter' }}>
+                                Notification
+                            </p>
+
+                            <IconButton size="small" aria-label="show help" color="inherit" onClick={closeAlertPage}>
+                                <CloseIcon sx={{color: '#212121', height: '25px', width: '25px'}} />
+                            </IconButton>
+
+                        </Box>
+
+                        <Box>
+
+                            <Box px={3} py={2} sx={{border: '1px solid #E0E0E0'}}>
+                                <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                    <p className="AlertTitle">
+                                        Admin assigned a New Case to you
+                                    </p>
+
+                                    <p className="AlertNewBage">
+                                        New
+                                    </p>
+                                </Box>
+                                <Button variant="outlined" sx={{color: '#1F1DAC', borderColor: '#1F1DAC'}}>
+                                    View
+                                </Button>
+                            </Box>
+
+                            <Box px={3} py={2} sx={{border: '1px solid #E0E0E0'}}>
+                                <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                    <p className="AlertTitle">
+                                        Admin assigned a New Case to you
+                                    </p>
+                                </Box>
+                                <Button variant="outlined" sx={{color: '#1F1DAC', borderColor: '#1F1DAC'}}>
+                                    View
+                                </Button>
+                            </Box>
+
+                            <Box px={3} py={2} sx={{border: '1px solid #E0E0E0'}}>
+                                <Box sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                                    <p className="AlertTitle">
+                                        Admin assigned a New Case to you
+                                    </p>
+                                </Box>
+                                <Button variant="outlined" sx={{color: '#1F1DAC', borderColor: '#1F1DAC'}}>
+                                    View
+                                </Button>
+                            </Box>
+
+                        </Box>
+
+                    </Box>
+                </Slide>
+            </>
+        }
+
+        </>
     );
 }
