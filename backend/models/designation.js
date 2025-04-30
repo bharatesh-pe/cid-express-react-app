8c9d02c6e3e8ba3b0717963e0802137feb655799
UV <uvkrishna05@gmail.com>
2025-04-30 12:30:54 +0530

    user update issue fixed

--- backend/models/designation.js
+++ backend/models/designation.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       department_id: {
         type: DataTypes.INTEGER,
-        allowNull: false,
+        allowNull: true,
       },
       created_by: {
         type: DataTypes.INTEGER,
