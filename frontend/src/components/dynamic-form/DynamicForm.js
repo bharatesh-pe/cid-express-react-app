da5e7f688ebdba3d86d73c63c1432ea204712545
Mukesh <mugesh415@gmail.com>
2025-05-06 14:06:53 +0530

    io issue

--- frontend/src/components/dynamic-form/DynamicForm.js
+++ frontend/src/components/dynamic-form/DynamicForm.js
@@ -208,17 +208,17 @@ const DynamicForm = ({
     
     const makeOrderCopyShow = (value) => {
 
-        const allFields = (stepperData && stepperData.length > 0) ? stepperConfigData : newFormConfig;
-
-        var updatedFormConfig = allFields.map((element) => {
-            if (element.name === "field_order_copy_(_17a_done_)") {
-                element.hide_from_ux = value; // true = hide, false = show
-            }
-            return element;
+        setNewFormConfig((prevFormConfig) => {
+            const updatedFormConfig = prevFormConfig.map((data) => {
+                if(data.name === "field_order_copy_(_17a_done_)"){
+                    return {...data, hide_from_ux : value }
+                }else{
+                    return data;
+                }
+            });
+            return updatedFormConfig;
         });
 
-        setNewFormConfig(updatedFormConfig);
-    
         if (value) {
             delete formData["field_order_copy_(_17a_done_)"];
         }
@@ -1242,7 +1242,7 @@ const DynamicForm = ({
     };
 
   //   console.log(stepperConfigData, "stepperConfigData stepperConfigData")
-  //   console.log(stepperData, "stepperData stepperData")
+  //   console.log(stepperData, "stepperData stepperData")  
   return (
     <>
       <Box
