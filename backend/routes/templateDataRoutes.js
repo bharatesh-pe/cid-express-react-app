25038ebe7465fc5651577ea624f209098cf24bc5
gayathridevirs <rsgayathridevi376@gmail.com>
2025-05-30 10:27:01 +0530

    backend for Nature of disposal final

--- backend/routes/templateDataRoutes.js
+++ backend/routes/templateDataRoutes.js
@@ -374,6 +374,8 @@ router.post('/submitActionPlanPR',[validate_token],
     templateDataController.submitActionPlanPR)
  router.post('/submitPropertyFormFSL',[validate_token],
     templateDataController.submitPropertyFormFSL)
+router.post('/checkCaseStatusByUiCaseId',[validate_token],
+   templateDataController.checkCaseStatusByUiCaseId)
    
         
 module.exports = router;
