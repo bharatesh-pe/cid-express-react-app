72bd79215042bbd77fb15c080fb260bdb6ef934f
UV <uvkrishna05@gmail.com>
2025-07-21 14:32:15 +0530

    police_app changes

--- backend/routes/activityLogRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const activityLogController = require('../controllers/activityLogController');
-const userAuthMiddleware = require('../middleware/userAuthMiddleware');
-const router = express.Router();
-
-
-router.post('/getActivityLog', userAuthMiddleware, activityLogController.getActivityLog)
-router.post('/paginateActivityLog', userAuthMiddleware, activityLogController.paginateActivityLog)
-module.exports = router;
--- backend/routes/mastersRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { get_master_data} = require('../controllers/masterController');
-const { validate_token } = require("../helper/validations")
-
-const router = express.Router();
-
-router.post('/get_master_data', [validate_token], get_master_data);
-
-module.exports = router;
