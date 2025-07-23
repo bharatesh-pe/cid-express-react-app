51c678b3aec19f26d5378876d7e64881bcd204d6
UV <uvkrishna05@gmail.com>
2025-07-23 09:25:39 +0530

    nature of disposal not saving

--- backend/controllers/templateDataController.js
+++ backend/controllers/templateDataController.js
@@ -8162,10 +8162,10 @@ exports.saveDataWithApprovalToTemplates = async (req, res, next) => {
                                     type: Sequelize.DataTypes.STRING,
                                     allowNull: true,
                                     },
-                                    publickey: {
-                                        type: Sequelize.DataTypes.STRING,
-                                        allowNull: true,
-                                    },
+                                    // publickey: {
+                                    //     type: Sequelize.DataTypes.STRING,
+                                    //     allowNull: true,
+                                    // },
                                 };
                            
                                 for (const field of PFschema) {
